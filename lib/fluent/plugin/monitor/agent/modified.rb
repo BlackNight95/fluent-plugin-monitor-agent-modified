f52e0bb9af8cbd3a11fa1c7cab3c7a142295a3e5
wanfeng.hu <wanfeng.hu@ihandysoft.com>
2019-08-14 14:15:05 +0800

    Create fluent-plugin-monitor-agent-modified

---
+++ lib/fluent/plugin/monitor/agent/modified.rb
@@ -0,0 +1,14 @@
+require "fluent/plugin/monitor/agent/modified/version"
+
+module Fluent
+  module Plugin
+    module Monitor
+      module Agent
+        module Modified
+          class Error < StandardError; end
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
